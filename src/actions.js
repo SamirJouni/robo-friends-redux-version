ac185181349400c54d2b6edaee9834b740e872e6
F1r3byte <f1r3byte@yandex.com>
2018-09-24 23:52:22 +0200

    added a new action

--- src/actions.js
+++ src/actions.js
@@ -10,3 +10,18 @@ export const setSearchField = text => ({
 	payload: text
 });
 
+export const requestRobots = dispatch => {
+	dispatch({
+		type: REQUEST_ROBOTS_PENDING
+	});
+	fetch("https://jsonplaceholder.typicode.com/users")
+		.then(response => response.json())
+		.then(users => dispatch({
+			type: REQUEST_ROBOTS_SUCCESS,
+			payload: users
+		}))
+		.catch(error => dispatch({
+			type: REQUEST_ROBOTS_FAILED,
+			payload: error
+		}));
+};
