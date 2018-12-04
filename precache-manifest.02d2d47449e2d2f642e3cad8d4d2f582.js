379f3766065a76303e11dc1721ad6f3cf653740f
F1r3byte <f1r3byte@yandex.com>
2018-12-04 23:45:29 +0100

    Updates

---
+++ precache-manifest.02d2d47449e2d2f642e3cad8d4d2f582.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df6c9dff6ce171a00bfd",
+    "url": "./static/css/main.e85b6cc3.chunk.css"
+  },
+  {
+    "revision": "df6c9dff6ce171a00bfd",
+    "url": "./static/js/main.df6c9dff.chunk.js"
+  },
+  {
+    "revision": "be7fa31b30408725a9e7",
+    "url": "./static/css/1.0b30536a.chunk.css"
+  },
+  {
+    "revision": "be7fa31b30408725a9e7",
+    "url": "./static/js/1.be7fa31b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "80cc053193546d5fb0552f4dfd7a4fce",
+    "url": "./index.html"
+  }
+];
