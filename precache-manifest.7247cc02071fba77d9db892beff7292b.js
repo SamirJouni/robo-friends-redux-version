5bee4fe7711cd4a987854eb6ac54dd10e4caf7f8
F1r3byte <f1r3byte@yandex.com>
2019-04-23 23:53:17 +0200

    Updates

---
+++ precache-manifest.7247cc02071fba77d9db892beff7292b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd30bd5f0f2c314e9cdfba53e0ed02a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30ce77472c4d4250057e",
+    "url": "./static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8d4148c1ecb9934ffa7b",
+    "url": "./static/css/main.33277fda.chunk.css"
+  },
+  {
+    "revision": "30ce77472c4d4250057e",
+    "url": "./static/js/2.9cde7e02.chunk.js"
+  },
+  {
+    "revision": "8d4148c1ecb9934ffa7b",
+    "url": "./static/js/main.78a0c794.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
